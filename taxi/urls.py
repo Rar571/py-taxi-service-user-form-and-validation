034082b7cd9237f842511e1db37764b3180e9feb
Ruslan <stadnikruslan168@gmail.com>
2026-02-28 23:24:18 +0200

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,3 +1,4 @@
+from django import views
 from django.urls import path
 
 from .views import (
@@ -12,7 +13,7 @@ from .views import (
     ManufacturerListView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
-    ManufacturerDeleteView,
+    ManufacturerDeleteView, DriverCreateView, DriverDeleteView, DriverLicenseUpdateView, car_assign_driver, car_remove_driver,
 )
 
 urlpatterns = [
@@ -42,10 +43,15 @@ urlpatterns = [
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
+    path('cars/<int:pk>/assign/', car_assign_driver, name='car-assign-driver'),
+    path('cars/<int:pk>/remove/', car_remove_driver, name='car-remove-driver'),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
+    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
+    path("drivers/<int:pk>/delete/", DriverDeleteView.as_view(), name="driver-delete"),
+    path("drivers_license/<int:pk>/update/", DriverLicenseUpdateView.as_view(), name="driver-license-update"),
 ]
 
 app_name = "taxi"
